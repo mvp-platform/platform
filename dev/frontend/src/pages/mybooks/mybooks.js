1de156e9e169eb27109f355e20333cb46a9d87e2
Josh Matteson <mattesoj@oregonstate.edu>
2017-04-14 13:45:18 -0700

    added basic book view

--- dev/frontend/src/pages/mybooks/mybooks.js
+++ dev/frontend/src/pages/mybooks/mybooks.js
@@ -4,7 +4,21 @@ import {HttpClient, json} from 'aurelia-fetch-client';
 let httpClient = new HttpClient();
 
 export class Books {
-    constructor() {}
+
+    constructor() {
+        this.titles = [];
+        httpClient.fetch('http://remix.ist:8000/books/hagrid')
+        .then(response => response.json())
+        .then(data => {
+            for(let instance of data) {
+                this.titles.push(instance.name);
+            }
+            console.log(this.titles);
+            this.books = data;
+        });
+
+        
+    }
     configureRouter(config, router) {
         config.title = 'Right Tabs';
         config.map([
@@ -15,12 +29,6 @@ export class Books {
         ]);
         this.router = router;
 
-        httpClient.fetch('http://remix.ist:8000/users/hagrid/books')
-        .then(response => response.json())
-        .then(data => {
-            // data = JSON.parse(data);
-            console.log(data);
-        });
     }
     
 }
