632b25c1bd87db03cc30c5299317a1f4b4d059c7
Josh Matteson <mattesoj@oregonstate.edu>
2017-04-14 16:11:00 -0700

    added some experimental st00f

--- dev/frontend/src/pages/mybooks/PDFViewer.js
+++ dev/frontend/src/pages/mybooks/PDFViewer.js
@@ -1,5 +1,5 @@
 export class PdfViewer {
-  constructor() {
+  constructor(bookID) {
     this.documents = [
       /*{
         url: 'src/documents/one.pdf',
@@ -9,13 +9,14 @@ export class PdfViewer {
         lastpage: 1
       },*/
       {
-        url: 'src/documents/two.pdf',
-        draftUrl: 'src/documents/two.pdf',
+        url: 'http://remix.ist:8000/books/hagrid/19b66178-856d-4dad-bbc2-a9575ecfd36b/pdf',
+        draftUrl: 'http://remix.ist:8000/books/hagrid/19b66178-856d-4dad-bbc2-a9575ecfd36b/pdf',
         pageNumber: 1,
         scale: 0.8,
         lastpage: 1
       }
     ];
+    //console.log(bookID);
   }
 
   loadUrl(document) {
@@ -54,5 +55,13 @@ export class PdfViewer {
 
   zoomOut(document) {
     document.scale = Number(document.scale) - 0.1;
+  }
+  activate(bookID) {
+    /*
+    let url = "http://remix.ist:8000/" + bookID;
+    document.url = url
+    document.draftUrl = url;
+    console.log(url);
+    */
   }
 }
--- dev/frontend/src/pages/mybooks/mybooks.js
+++ dev/frontend/src/pages/mybooks/mybooks.js
@@ -6,16 +6,15 @@ let httpClient = new HttpClient();
 export class Books {
 
     constructor() {
-        
+
         this.books = [];
 
         httpClient.fetch('http://remix.ist:8000/books/hagrid')
         .then(response => response.json())
         .then(data => {
             for(let instance of data) {
-                this.books.push(instance);
-
                 console.log(instance);
+                this.books.push(instance);
             }
 
         });
@@ -25,7 +24,7 @@ export class Books {
     configureRouter(config, router) {
         config.title = 'Right Tabs';
         config.map([
-            { route: ['', 'PDF Viewer'], name: 'PDFViewer', moduleId: 'pages/mybooks/PDFViewer', nav: true, title: 'PDF Viewer'},
+            { route: ['', '/:bookID'], name: 'PDFViewer', moduleId: 'pages/mybooks/PDFViewer', nav: true, title: 'PDF Viewer'},
             { route: 'account', name: 'account', moduleId: 'pages/mybooks/account', nav: true, title: 'Account' },
             { route: 'emails', name: 'emails', moduleId: 'pages/mybooks/emails', nav: true, title: 'Emails' },
             { route: 'notifications', name: 'notifications', moduleId: 'pages/mybooks/notifications', nav: true, title: 'Notifications' }
