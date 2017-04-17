a65346a0a624ca5d0926656df435b94c4a5c8450
Evan Tschuy <evan.tschuy@coreos.com>
2017-04-17 16:55:27 -0700

    add login pgae

--- src/accounts.js
+++ src/accounts.js
@@ -22,6 +22,31 @@ const verifylogin = async function(request) {
   return {success: true, username: user_blob[0].userid};
 };
 
+const loginPage = async function(request, reply) {
+  return reply(`<html lang="en">
+  <head>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
+    <meta name="google-signin-scope" content="profile email">
+    <meta name="google-signin-client_id" content="643825511576-aecm78ba0gdc5aild94hi0on5lrrobma.apps.googleusercontent.com">
+    <script src="https://apis.google.com/js/platform.js" async defer></script>
+  </head>
+  <body>
+    <div class="g-signin2" data-onsuccess="onSignIn" data-theme="dark"></div>
+    <script>
+      var SERVER = "http://remix.ist:8000";
+      function onSignIn(googleUser) {
+        // The ID token you need to pass to your backend:
+        var id_token = googleUser.getAuthResponse().id_token;
+        $.post( SERVER + "/accounts/login", {token: id_token}, function( data ) {
+          console.log(data);
+        });
+      };
+    </script>
+  </body>
+</html>
+`)
+}
+
 // /accounts/login
 const login = async function(request, reply) {
   var auth = new GoogleAuth;
@@ -44,7 +69,11 @@ const routes = [{
     method: 'POST',
     path: '/accounts/login',
     handler: login
-  }
+  }, {
+      method: 'GET',
+      path: '/login',
+      handler: loginPage
+    }
 ];
 
 const register = function(server) {
