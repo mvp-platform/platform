c591d0a0b1a34a48a324a02b4d8f7b2bc9cf1ffe
Evan Tschuy <evan.tschuy@coreos.com>
2017-04-24 22:50:49 -0700

    add some packages

--- dev/backend/src/scraps.js
+++ dev/backend/src/scraps.js
@@ -12,6 +12,18 @@ const lescape = require('escape-latex');
 const scrapTmpl = `
 
 \\documentclass{article}
+\\usepackage{graphicx}
+\\usepackage{setspace}
+\\usepackage{comment}
+\\usepackage{bigstrut}
+\\usepackage{geometry}
+\\usepackage{supertabular}
+\\usepackage{tabu}
+\\usepackage{hyperref}
+\\usepackage{url}
+\\hypersetup{
+  colorlinks=true, linkcolor=blue, citecolor=blue, filecolor=blue, urlcolor=blue}
+\\geometry{textheight=9.5in, textwidth=7in}
 \\usepackage{fontspec}
 \\defaultfontfeatures{Ligatures=TeX}
 \\usepackage[small,sf,bf]{titlesec}
