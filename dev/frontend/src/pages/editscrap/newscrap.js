fd6082631c5c77b228e0f338b1b56f055ba1730f
Steven Powers <powersst@oregonstate.edu>
2017-04-25 22:15:00 -0700

    newScrap to Chapter
    Scraps now append to the end of the chapter provided by the bound
    parameters.

--- dev/frontend/src/pages/editscrap/newscrap.js
+++ dev/frontend/src/pages/editscrap/newscrap.js
@@ -1,5 +1,6 @@
 import 'fetch';
 import { HttpClient, json } from 'aurelia-fetch-client';
+import {Cookies} from 'aurelia-plugins-cookies';
 
 let httpClient = new HttpClient();
 
@@ -7,74 +8,32 @@ export class Scraps {
 
     constructor(author) {
         console.log("constructor");
-        // this.scraps = [];
-        // //
-        // // //' + chapterID.author + '/' + chapterID.uuid)
-        // // httpClient.fetch('http://remix.ist:8000/scraps/hagrid')
-        // // .then(response => response.json())
-        // // .then(data => {
-        // //     for(let instance of data) {
-        // //         console.log(instance);
-        // //         this.scraps.push(instance);
-        // //     }
-        // //
-        // //
-        // // });
-        //
-        //
-        // httpClient.fetch('http://remix.ist:8000/scraps/hagrid/4071b78c-2328-4a59-a278-86200f93bfde')
-        // .then(response => response.json())
-        // .then(data => {
-        //     console.log(data);
-        //     //this.chapters.push(data);
-        //
-        //     for(let instance of data) {
-        //         console.log(instance);
-        //         this.scraps.push(instance);
-        //     }
-        //
-        // });
 
     }
 
-
-    // email = '';
-    // password = '';
     userText = '';
+    enableLatex = false;
 
     submitNewScrap() {
+        console.log(Cookies.get('data'));
         var requested = this.userText;
-        var theAuthor = this.chapters[0];
+        var enableLatex = this.enableLatex;
+        var theAuthor = Cookies.get('username');
         var theChapter = this.chapters[1];
-        var authToken = 'Token abc123';
+        var authToken = "Token " + Cookies.get('token');
 
         console.log(requested);
         console.log(theAuthor);
         console.log(theChapter);
+        console.log(enableLatex);
 
 
-        //alert(userText);
-        // this.http = this.httpClient();
-        //BookID
-        //f12d3550-b93e-455f-8d41-fbd480f464bb
-        //chapterID
-        //439385c0-66e0-46c3-8390-36805da3154c
-        //scrapID
-        //4071b78c-2328-4a59-a278-86200f93bfde
         if(theChapter == undefined || theChapter == null || theChapter == "")
         {
             let request = {
-                //   //name: "Hagrid's First Chapter",
-                author: theAuthor,
-                //   //FOR SETTING CHAPTER
-                //   // chapters: [
-                //   //   [
-                //   //   "hagrid",
-                //   //   "439385c0-66e0-46c3-8390-36805da3154c"
-                //   //   ]
-                //   // ]
-                //   text: "\\section{Testing}This is a new UPDATED scrap created with posting via the newscrap page!"
-                text: requested
+              author: theAuthor,
+              text: requested,
+              latex: enableLatex
             };
 
             //CREATE THE NEW SCRAP AND GET THE NEW SCRAP ID
@@ -84,7 +43,7 @@ export class Scraps {
             //         body: JSON.stringify(request),
             //         headers: {
             //             'Content-Type': 'application/json',
-            //             'Authorization': 'Token abc123'
+            //             'Authorization': authToken
             //         }
             //     })
             //     .then(response => response.json())
@@ -100,23 +59,15 @@ export class Scraps {
         else //The Chapter Was Provided, so create the scrap and then add it to a chapter.
         {
           let request = {
-              //   //name: "Hagrid's First Chapter",
               author: theAuthor,
-              //   //FOR SETTING CHAPTER
-              //   // chapters: [
-              //   //   [
-              //   //   "hagrid",
-              //   //   "439385c0-66e0-46c3-8390-36805da3154c"
-              //   //   ]
-              //   // ]
-              //   text: "\\section{Testing}This is a new UPDATED scrap created with posting via the newscrap page!"
-              text: requested
+              text: requested,
+              latex: enableLatex
           };
 
 
-          // CREATE THE NEW SCRAP AND GET THE NEW SCRAP ID
+          //CREATE THE NEW SCRAP AND GET THE NEW SCRAP ID
           var scrapID = '';
-          httpClient.fetch('http://remix.ist:8000/scraps/new', {
+          httpClient.fetch('http://remix.ist/scraps/new', {
                   method: 'post',
                   body: JSON.stringify(request),
                   headers: {
@@ -126,25 +77,11 @@ export class Scraps {
               })
               .then(response => response.json())
               .then(data => {
-                  console.log("response: ");
-                  scrapID = data.uuid;
+                  console.log(data);
+                  var scrapID = data.uuid;
+
                   console.log(scrapID);
 
-                  //RECOVER CHAPTERS SCRAPS
-                  // let request3 = {
-                  //     //   //name: "Hagrid's First Chapter",
-                  //     //author: theAuthor,
-                  //     //   //FOR SETTING CHAPTER
-                  //     chapters:
-                  //     [
-                  //         [
-                  //         theAuthor,
-                  //         theChapter
-                  //         ]
-                  //     ]
-                  //     //   text: "\\section{Testing}This is a new UPDATED scrap created with posting via the newscrap page!"
-                  //     text: requested
-                  // };
 
                   //var scrapID = "06776465-ab55-45b1-9a8e-5a379bb903b2";
 
@@ -178,30 +115,30 @@ export class Scraps {
                               //alert(newScrapRequest.toString());
 
                               let request2 = {
-                                  //   //name: "Hagrid's First Chapter",
                                   scraps:
                                       newScrapRequest
 
                               };
 
-                              alert(JSON.stringify(request2));
+                              //alert(JSON.stringify(request2));
 
                               //console.log(this.scraps);
                               //alert(this.scraps);
 
-                              // httpClient.fetch('http://remix.ist:8000/chapters/' + theAuthor + '/' + theChapter, {
-                              //         method: 'post',
-                              //         body: JSON.stringify(request2),
-                              //         headers: {
-                              //             'Content-Type': 'application/json',
-                              //             'Authorization': authToken
-                              //         }
-                              //     })
-                              //     .then(response => response.json())
-                              //     .then(data => {
-                              //         console.log(data)
-                              //         alert("Scrap has been added to chapter " + theChapter);
-                              //     });
+                              //   UPDATE CHAPTER WITH NEW SCRAPS
+                              httpClient.fetch('http://remix.ist/chapters/' + theAuthor + '/' + theChapter, {
+                                      method: 'post',
+                                      body: JSON.stringify(request2),
+                                      headers: {
+                                          'Content-Type': 'application/json',
+                                          'Authorization': authToken
+                                      }
+                                  })
+                                  .then(response => response.json())
+                                  .then(data => {
+                                      console.log(data)
+                                      alert("Scrap has been added to chapter " + theChapter);
+                                  });
 
 
                       });
@@ -211,24 +148,6 @@ export class Scraps {
 
         }
 
-
-
-        //console.log(this.scraps);
-        //alert(this.scraps);
-
-        //
-        // httpClient.fetch('http://remix.ist:8000/scraps/new', {
-        //         method: 'post',
-        //         body: JSON.stringify(request),
-        //         headers: {
-        //             'Content-Type': 'application/json',
-        //             'Authorization': 'Token abc123'
-        //         }
-        //     })
-        //     .then(response => response.json())
-        //     .then(data => {
-        //         console.log(data)
-        //     });
 
     }
 
@@ -250,23 +169,14 @@ export class Scraps {
       this.chapters.push(chapter);
 
       console.log(this.chapters);
-      // httpClient.fetch('http://remix.ist:8000/chapters/' + author.author + '/' + author.uuid)
-      //     .then(response => response.json())
-      //     .then(data => {
-      //         console.log(data);
-      //         this.chapters.push(data);
-      //     });
-
-
-
     }
 
     configureRouter(config, router) {
         config.map([
             { route: ['', ':author/:uuid'], name: 'PDFViewer', moduleId: 'pages/editscrap/PDFViewer', nav: false, title: 'PDF Viewer' },
-            { route: ['', 'search'], name: 'search', moduleId: 'pages/editscrap/search', nav: true, title: 'search' },
-            { route: 'emails', name: 'emails', moduleId: 'pages/editscrap/emails', nav: true, title: 'Emails' },
-            { route: 'notifications', name: 'notifications', moduleId: 'pages/editscrap/notifications', nav: true, title: 'Notifications' }
+            { route: ['', 'search'], name: 'Search', moduleId: 'pages/editscrap/search', nav: true, title: 'Search' }
+            //{ route: 'emails', name: 'emails', moduleId: 'pages/editscrap/emails', nav: true, title: 'Emails' },
+            //{ route: 'notifications', name: 'notifications', moduleId: 'pages/editscrap/notifications', nav: true, title: 'Notifications' }
         ]);
         this.router = router;
     }
