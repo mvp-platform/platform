66f9591cb4721986ccd7e4183d860a32e6f0e1e1
Steven Powers <powersst@oregonstate.edu>
2017-04-26 16:08:33 -0700

    Profile Options & Settings Options
    Updated profile options page to allow the user to edit the name that
    they want to have appear on their published materials.
    
    Also built out a dummy settings page, but we need to come up with
    settings if we are going to keep this page.

--- dev/frontend/src/pages/profile.js
+++ dev/frontend/src/pages/profile.js
@@ -1,3 +1,77 @@
+import 'fetch';
+import {HttpClient, json} from 'aurelia-fetch-client';
+import {Cookies} from 'aurelia-plugins-cookies';
+import { inject } from 'aurelia-framework';
+import { MdToastService } from 'aurelia-materialize-bridge';
+
+let httpClient = new HttpClient();
+
+@inject(MdToastService)
 export class Profile {
-    constructor() {}
+    constructor(toast) {
+      this.toast = toast;
+    }
+
+    updateUserDisplayName(){
+      //Variables for updating
+      var username = '';
+      var displayname = '';
+      var authToken = '';
+
+      username = this.userName; //Could retrieve from the cookie, but this is already done to load the page
+      displayname = this.userDisplayName;
+      authToken = "Token " + Cookies.get('token');
+
+      if(displayname !== null || displayname !== "" || displayname !== undefined) {
+        var request = {
+            name: displayname
+        };
+
+        httpClient.fetch('http://remix.ist/accounts/myaccount', {
+                    method: 'post',
+                    body: JSON.stringify(request),
+                    headers: {
+                        'Content-Type': 'application/json',
+                        'Authorization': authToken
+                    }
+        })
+        .then(response => response.json())
+        .then(data => {
+                console.log(data);
+                this.toast.show('Display name updated successfully!', 5000, 'rounded orange');
+
+        });
+      }
+
+    }
+
+
+    activate() {
+      var username = '';
+      var authToken = '';
+      //this.userText = '';
+
+      username = Cookies.get('username');
+      authToken = "Token " + Cookies.get('token');
+
+      this.userInfo = [];
+      this.userInfo.push(username);
+
+      console.log(this.userInfo);
+
+      httpClient.fetch('http://remix.ist/accounts/myaccount', {
+                  headers: {
+                      'Content-Type': 'application/json',
+                      'Authorization': authToken
+                  }
+      })
+      .then(response => response.json())
+      .then(data => {
+              console.log(data);
+              this.userName = data.userid;
+              this.userDisplayName = data.name;
+
+      });
+
+    }
 }
